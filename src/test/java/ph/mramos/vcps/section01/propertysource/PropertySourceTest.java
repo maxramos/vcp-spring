f71401e40e23809628ea095b3cfb2145f137efd6
Max <Max@MAX-LENOVO>
2021-10-15 19:56:37 +0800

    Updated property source test.

--- src/test/java/ph/mramos/vcps/section01/propertysource/PropertySourceTest.java
+++ src/test/java/ph/mramos/vcps/section01/propertysource/PropertySourceTest.java
@@ -2,9 +2,13 @@ package ph.mramos.vcps.section01.propertysource;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 @SpringJUnitConfig(classes = SamplePropertySourceConfig.class)
+@TestPropertySource(
+		locations = "classpath:/config/application-test.properties", // Overrides value in application.properties
+		properties = "sample.key.web=Sample Test Key Web") // Overrides value in web.properties
 public class PropertySourceTest {
 
 	@Autowired
